0657f26aaa113ee174c9026bc816d2850e3b4fdd
sue <ysh@knu.ac.kr>
2025-07-21 17:41:26 +0900

    refactor: 아이콘 컴포넌트화

---
+++ frontend/src/components/icons/RoutineIcon.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+interface RoutineIconProps {
+  isSelected: boolean
+}
+
+const RoutineIcon: React.FC<RoutineIconProps> = ({ isSelected }) => {
+  const color = isSelected ? 'var(--main)' : 'var(--icon)'
+
+  return (
+    <svg width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1.875 16.208H3.3125" stroke={color} strokeWidth="2" strokeLinecap="round" />
+      <path d="M7.146 16.208H19.1252" stroke={color} strokeWidth="2" strokeLinecap="round" />
+      <path d="M1.875 10.458H3.3125" stroke={color} strokeWidth="2" strokeLinecap="round" />
+      <path d="M7.146 10.458H19.1252" stroke={color} strokeWidth="2" strokeLinecap="round" />
+      <path d="M10.021 4.70801H19.1252" stroke={color} strokeWidth="2" strokeLinecap="round" />
+      <path
+        d="M1.875 4.70801L3.79167 6.62467L8.58333 1.83301"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default RoutineIcon
---
+++ frontend/src/components/icons/UploadIcon.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+interface UploadIconProps {
+  isSelected: boolean
+}
+
+const UploadIcon: React.FC<UploadIconProps> = ({ isSelected }) => {
+  const color = isSelected ? 'var(--main)' : 'var(--icon)'
+
+  return (
+    <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M3.38428 6.625L8.42717 1.58211C8.81769 1.19158 9.45086 1.19158 9.84138 1.58211L14.8843 6.625"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.13428 1.87988V13.3799"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <line
+        x1="1.875"
+        y1="17.1885"
+        x2="17.125"
+        y2="17.1885"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  )
+}
+
+export default UploadIcon
