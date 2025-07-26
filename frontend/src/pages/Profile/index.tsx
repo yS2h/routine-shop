45e98a4323f195c4be6ac1e2ffa4084ba8f3cdae
swk <sw.k00105@gmail.com>
2025-07-26 22:26:49 +0900

    프로필화면 UI V1.0.0

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,3 +1,14 @@
+import { useEffect, useState } from "react";
+import Login from "@/pages/Login"; // 로그인 화면 컴포넌트
+import MyProfile from "./MyProfile"; // 기존의 프로필 화면
+
 export default function Profile() {
-  return <div> 프로필</div>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const loggedIn = localStorage.getItem("isLoggedIn") === "true";
+    setIsLoggedIn(loggedIn);
+  }, []);
+
+  return isLoggedIn ? <MyProfile /> : <Login />;
 }
