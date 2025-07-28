26af68ae978eac7e7022362753d602c7f63c058d
sue <ysh@knu.ac.kr>
2025-07-28 18:05:51 +0900

    feat: 구매 페이지 UI 뼈대

---
+++ frontend/src/pages/Home/Detail.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header'
+
+export default function Purchase() {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-[360px] px-4 relative min-h-screen space-y-6">
+        <Header />
+      </div>
+    </div>
+  )
+}
