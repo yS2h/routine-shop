35c5b0b0a7f536090d915f8ecd63cdf35ea7dd11
sue <ysh@knu.ac.kr>
2025-07-28 10:07:39 +0900

    fix: 검색 컴포넌트 범위 수정

--- frontend/src/components/Search/index.tsx
+++ frontend/src/components/Search/index.tsx
@@ -2,15 +2,15 @@ import { Input } from '@/components/ui/input'
 
 const Search = () => {
   return (
-    <div className="absolute top-[98px] relative flex w-[360px] h-[36px] items-center">
+    <div className="absolute top-[98px] relative w-full max-w-[360px] h-[36px] items-center">
       {' '}
       <img
         src="/search.svg"
         alt="search"
-        className="absolute right-9 top-1/2 transform -translate-y-1/2 h-5 w-5"
+        className="absolute right-6 top-1/2 transform -translate-y-1/2 h-5 w-5"
       />
       <Input
-        className="w-[340px] h-full rounded-[10px] border-[0.5px] py-1.5 pr-10 pl-3.5 bg-white placeholder:text-gray-400"
+        className="w-full h-full rounded-[10px] border-[0.5px] py-1.5 pr-10 pl-3.5 bg-white placeholder:text-gray-400"
         type="search"
         style={{
           borderColor: 'var(--stroke)',
